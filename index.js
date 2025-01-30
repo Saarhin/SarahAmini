68750a65885ed211a04c6db7b377973211e419e5
Saarhin <sarah77.amini@gmail.com>
2025-01-30 15:20:22 -0700

    Add new page

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ function populateRepo(items, id) {
 
     // Make the card clickable by wrapping the content inside an anchor tag
     const repoLink = document.createElement("a");
-    repoLink.href = `https://saarhin.github.io/SarahAmini/title?repo=${encodeURIComponent(items[i].name)}`;
+    repoLink.href = `https://saarhin.github.io/SarahAmini/RepoDetails`;
     repoLink.target = "_blank";
     repoLink.style =
       "text-decoration: none; color: black; display: block; height: 100%;";
