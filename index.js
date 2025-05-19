e31a85b5aa8212502343d9e44037936f102392fa
saarhin <sarah77.amini@gmail.com>
2025-05-19 16:31:20 -0600

    pageName bug fixed

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ function populateRepo(items, id) {
 
     // Make the card clickable by wrapping the content inside an anchor tag
     const repoLink = document.createElement("a");
-    repoLink.href = `https://saarhin.github.io/SarahAmini/${items[i].repoName}`;
+    repoLink.href = `https://saarhin.github.io/SarahAmini/${items[i].pageName}`;
     repoLink.target = "_blank";
     repoLink.style =
       "text-decoration: none; color: black; display: block; height: 100%;";
